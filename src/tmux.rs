cf3b6f22716d3025a5081f0dfc622f08866bbaa8
Michael Stock <mikeastock@gmail.com>
2017-04-19 22:26:11 -0700

    refactor: Cleanup tmux background flashing

--- src/tmux.rs
+++ src/tmux.rs
@@ -1,15 +1,20 @@
 use error::Error;
 use std::process::{Command,ExitStatus};
+use std::thread::sleep;
+use std::time::Duration;
 use super::Result;
 
 const RED: &'static str = "red";
 const BLACK: &'static str = "black";
 
 pub fn flash_background() -> Result<()> {
-    set_background(RED)?;
-    set_background(BLACK)?;
-    set_background(RED)?;
-    set_background(BLACK)?;
+    for _ in 0..3 {
+        set_background(RED)?;
+        sleep(Duration::from_millis(300));
+        set_background(BLACK)?;
+        sleep(Duration::from_millis(300));
+    }
+
     Ok(())
 }
 
@@ -17,7 +22,7 @@ fn set_background(color: &str) -> Result<ExitStatus> {
     Command::new("tmux")
         .arg("select-pane")
         .arg("-P")
-        .arg(format!("'bg={}'", color))
+        .arg(format!("bg={}", color))
         .status()
         .map_err(|error| Error::Io(error))
 }
