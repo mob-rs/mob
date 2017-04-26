61ad799b334ce5b79b6d7a9fc8f3041f7ef0d8e5
Michael Stock <mikeastock@gmail.com>
2017-04-25 21:10:34 -0700

    feat: Open new window and prompt user to continue

--- src/timer.rs
+++ src/timer.rs
@@ -1,8 +1,9 @@
 use std::thread::sleep;
 use std::time::Duration;
 use super::Result;
-use team::{Member,Team};
+use team::Team;
 use tmux;
+use std::process::exit;
 
 pub fn run(time_per_driver_in_minutes: &f64, team: &mut Team) -> Result<()> {
     let time_per_driver_in_seconds = time_per_driver_in_minutes * 60.0;
@@ -11,9 +12,7 @@ pub fn run(time_per_driver_in_minutes: &f64, team: &mut Team) -> Result<()> {
 
     loop {
         if is_time_for_next_driver(&time_per_driver_in_seconds, elapsed_time) {
-            team.next_driver();
-            tmux::flash_background()?;
-            tmux::send_message(next_driver_message(&team.driver))?;
+            prompt_user(team)?;
         };
 
         println!("{}", team.driver);
@@ -22,14 +21,27 @@ pub fn run(time_per_driver_in_minutes: &f64, team: &mut Team) -> Result<()> {
     }
 }
 
+fn prompt_user(team: &mut Team) -> Result<()> {
+    let prompt_command = "sleep 1; echo continue > /tmp/mob";
+
+    let exit_status = tmux::new_window_with_command(prompt_command)?;
+
+    if exit_status.success() && is_continue() {
+        team.next_driver();
+        Ok(())
+    } else {
+        exit(1);
+    }
+}
+
+fn is_continue() -> bool {
+    true
+}
+
 fn is_time_for_next_driver(time_per_driver: &f64, elapsed_time: f64) -> bool {
     if elapsed_time != 0.0 && elapsed_time % time_per_driver == 0.0 {
         true
     } else {
         false
     }
-}
-
-fn next_driver_message(driver: &Member) -> String {
-    format!("Next Driver: {}", driver)
 }
