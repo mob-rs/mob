4e2cefbeb749564b911f6717da9f219a14cf7e0b
Michael Stock <mikeastock@gmail.com>
2017-05-07 22:57:04 -0700

    chore: Try setting rust channel

--- ci/script.sh
+++ ci/script.sh
@@ -4,18 +4,18 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cross build --target $TARGET --all
+    cross build --target $TARGET --all --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cross test --target $TARGET --all
+    cross test --target $TARGET --all --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    cross run --target $TARGET --all
+    cross run --target $TARGET --all --release
 }
 
 # we don't run the "test phase" when doing deploys
