75dd54c11b28f4d9de581a78509952f80e91705d
Michael Stock <mikeastock@gmail.com>
2017-08-15 22:04:21 +0000

    fix: Don't infer schema as .env is unavailable in release

--- server/src/schema.rs
+++ server/src/schema.rs
@@ -1,1 +1,17 @@
-infer_schema!("dotenv:DATABASE_URL");
+table! {
+    teams {
+        id -> Integer,
+        time -> Double,
+    }
+}
+
+table! {
+    members {
+        id -> Integer,
+        name -> Text,
+        team_id -> Integer,
+        active -> Bool,
+        driver -> Bool,
+        position -> Integer,
+    }
+}
