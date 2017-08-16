e95876e6f22d1162744f23adcd0c1815e2de189c
Michael Stock <mikeastock@gmail.com>
2017-08-16 03:15:06 +0000

    docs: Remove comments

--- cli/src/tmux.rs
+++ cli/src/tmux.rs
@@ -1,16 +1,6 @@
 use errors::Result;
 use std::process::{Command, ExitStatus};
 
-// Open new window
-// Run mob prompt
-//      Write result to file
-// Return input from user as stdout
-// Parse input ->
-//      if continue
-//          set new driver
-//      else
-//          exit
-//      end
 pub fn new_window_with_command(command: &str) -> Result<ExitStatus> {
     Command::new("tmux")
         .arg("new-window")
